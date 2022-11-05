7de00904d7582d9dd66936f9529354373e6fec1b
Charity Yohn <53451923+CYohn@users.noreply.github.com>
2022-11-04 21:49:08 -0400

    First Build

---
+++ app/src/main/java/com/zybooks/c196_abm2_charity_yohn/MainActivity.java
@@ -0,0 +1,14 @@
+package com.zybooks.c196_abm2_charity_yohn;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
