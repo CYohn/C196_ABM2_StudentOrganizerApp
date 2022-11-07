442d442714675d0c669db29c36ec9e2923ccaeb0
Charity Yohn <53451923+CYohn@users.noreply.github.com>
2022-11-07 00:11:28 -0500

    Added labels in the manifest for courses and assessments (to change the viewable title of the activity). Added a menu resource in resources. Started the code for the menu in the AllTermsList.java

--- app/src/main/java/UI/AllTermsList.java
+++ app/src/main/java/UI/AllTermsList.java
@@ -1,6 +1,8 @@
 package UI;
 
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -12,5 +14,20 @@ public class AllTermsList extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_all_terms_list);
+
+    }
+    public boolean onCreateOptionsMenu(Menu menu){
+        //inflate the menu; this adds items to the action bar if it is present
+        getMenuInflater().inflate(R.menu.menu_terms_list, menu);
+        return true;
+    }
+
+    public boolean onOptionsItemSelection(MenuItem item){
+        switch (item.getItemId()){
+            case android.R.id.home:
+                this.finish();
+                return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
