669183f8094aaa46c39c2afb2dc04c9010f3b4fa
Charity Yohn <53451923+CYohn@users.noreply.github.com>
2022-11-17 21:51:45 -0500

    Completed the layout for fragment_add_assessment.xml. Worked on the layout for AssessmentsActivity. Modified the New Course Layout to fit the Nexus 5.

--- app/src/main/java/UI/AssessmentsActivity.java
+++ app/src/main/java/UI/AssessmentsActivity.java
@@ -1,8 +1,10 @@
 package UI;
 
 import android.os.Bundle;
+import android.view.View;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentTransaction;
 
 import com.zybooks.c196_abm2_charity_yohn.R;
 
@@ -12,5 +14,20 @@ public class AssessmentsActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_assessments);
+    }
+
+    public void pressedListAllAssessmentsBtn(View view) {
+        FragmentTransaction fragmentTransaction = getSupportFragmentManager().beginTransaction();
+        fragmentTransaction.replace(R.id.assessmentFragmentContainerView, new AllAssessmentsListFragment());
+        fragmentTransaction.addToBackStack("AllAssessmentsListFragment");
+        fragmentTransaction.commit();
+    }
+
+
+    public void pressedOpenAddAssessmentFragBtn(View view) {
+        FragmentTransaction fragmentTransaction = getSupportFragmentManager().beginTransaction();
+        fragmentTransaction.replace(R.id.assessmentFragmentContainerView, new AddAssessmentFragment());
+        fragmentTransaction.addToBackStack("AddAssessmentFragmentView");
+        fragmentTransaction.commit();
     }
 }
