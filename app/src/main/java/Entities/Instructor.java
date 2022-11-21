87288467f94679883a62e5d310e6c076c56fa719
Charity Yohn <53451923+CYohn@users.noreply.github.com>
2022-11-20 20:01:44 -0500

    Completed the Instructor.java entity object.

--- app/src/main/java/Entities/Instructor.java
+++ app/src/main/java/Entities/Instructor.java
@@ -1,4 +1,48 @@
 package Entities;
 
+import androidx.room.Entity;
+import androidx.room.PrimaryKey;
+
+@Entity(tableName = "instructors")
 public class Instructor {
+
+    @PrimaryKey(autoGenerate = true)
+
+    //private int instructorPrimaryKey;
+    private String instructorName;
+    private String instructorEmail;
+    private String instructorPhone;
+
+    @Override
+    public String toString() {
+        return "Instructor{" +
+                "instructorName=" + instructorName +
+                ", instructorEmail='" + instructorEmail + '\'' +
+                ", instructorPhone=" + instructorPhone +
+                '}';
+    }
+
+    public String getInstructorName() {
+        return instructorName;
+    }
+
+    public void setInstructorName(String instructorName) {
+        this.instructorName = instructorName;
+    }
+
+    public String getInstructorEmail() {
+        return instructorEmail;
+    }
+
+    public void setInstructorEmail(String instructorEmail) {
+        this.instructorEmail = instructorEmail;
+    }
+
+    public String getInstructorPhone() {
+        return instructorPhone;
+    }
+
+    public void setInstructorPhone(String instructorPhone) {
+        this.instructorPhone = instructorPhone;
+    }
 }
