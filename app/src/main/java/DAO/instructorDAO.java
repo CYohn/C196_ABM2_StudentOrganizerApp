138ca45e74bf94fa1c398ec97f3488b7c3379a52
Charity Yohn <53451923+CYohn@users.noreply.github.com>
2022-11-20 21:41:00 -0500

    Completed InstructorDAO: Insert, Update, Delete, Query

--- app/src/main/java/DAO/instructorDAO.java
+++ app/src/main/java/DAO/instructorDAO.java
@@ -1,4 +1,26 @@
 package DAO;
 
+import androidx.room.Delete;
+import androidx.room.Insert;
+import androidx.room.OnConflictStrategy;
+import androidx.room.Query;
+import androidx.room.Update;
+
+import java.util.List;
+
+import Entities.Instructor;
+
 public interface instructorDAO {
+    @Insert(onConflict= OnConflictStrategy.IGNORE)
+    void insert(Instructor instructor);
+
+    @Update
+    void update(Instructor instructor);
+
+    @Delete
+    void delete(Instructor instructor);
+
+    @Query("SELECT * FROM INSTRUCTORS ORDER BY instructorName ASC")
+    List<Instructor> getAllInstructors();
+
 }
