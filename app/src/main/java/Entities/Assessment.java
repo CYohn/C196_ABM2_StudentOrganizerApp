bc7eae5890a598f8c7d0d17d6d342b0c902926d7
Charity Yohn <53451923+CYohn@users.noreply.github.com>
2022-11-20 21:22:38 -0500

    Created the Assessment object: variables, constructor, setters/getters

--- app/src/main/java/Entities/Assessment.java
+++ app/src/main/java/Entities/Assessment.java
@@ -1,4 +1,54 @@
 package Entities;
 
+
+import androidx.room.Entity;
+import androidx.room.PrimaryKey;
+
+@Entity (tableName = "assessments")
 public class Assessment {
+
+    @PrimaryKey(autoGenerate = true)
+    private String assessmentType;
+    private String assessmentTitle;
+    private String assessmentEndDate;
+    private String assessmentStartDate;
+
+    public Assessment(String assessmentType, String assessmentTitle, String assessmentEndDate, String assessmentStartDate) {
+        this.assessmentType = assessmentType;
+        this.assessmentTitle = assessmentTitle;
+        this.assessmentEndDate = assessmentEndDate;
+        this.assessmentStartDate = assessmentStartDate;
+    }
+
+    public String getAssessmentType() {
+        return assessmentType;
+    }
+
+    public void setAssessmentType(String assessmentType) {
+        this.assessmentType = assessmentType;
+    }
+
+    public String getAssessmentTitle() {
+        return assessmentTitle;
+    }
+
+    public void setAssessmentTitle(String assessmentTitle) {
+        this.assessmentTitle = assessmentTitle;
+    }
+
+    public String getAssessmentEndDate() {
+        return assessmentEndDate;
+    }
+
+    public void setAssessmentEndDate(String assessmentEndDate) {
+        this.assessmentEndDate = assessmentEndDate;
+    }
+
+    public String getAssessmentStartDate() {
+        return assessmentStartDate;
+    }
+
+    public void setAssessmentStartDate(String assessmentStartDate) {
+        this.assessmentStartDate = assessmentStartDate;
+    }
 }
