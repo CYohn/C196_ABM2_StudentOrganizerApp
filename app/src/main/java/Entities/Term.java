af2061089a87cb7908a27bc531b579442a022be3
Charity Yohn <53451923+CYohn@users.noreply.github.com>
2022-11-20 21:26:12 -0500

    Created the Term object: variables, constructor, setters/getters

--- app/src/main/java/Entities/Term.java
+++ app/src/main/java/Entities/Term.java
@@ -1,4 +1,44 @@
 package Entities;
 
+
+import androidx.room.Entity;
+import androidx.room.PrimaryKey;
+
+@Entity(tableName = "terms")
 public class Term {
+
+    @PrimaryKey(autoGenerate = true)
+    private String termTitle;
+    private String termEndDate;
+    private String termStartDate;
+
+    public Term(String termTitle, String termEndDate, String termStartDate) {
+        this.termTitle = termTitle;
+        this.termEndDate = termEndDate;
+        this.termStartDate = termStartDate;
+    }
+
+    public String getTermTitle() {
+        return termTitle;
+    }
+
+    public void setTermTitle(String termTitle) {
+        this.termTitle = termTitle;
+    }
+
+    public String getTermEndDate() {
+        return termEndDate;
+    }
+
+    public void setTermEndDate(String termEndDate) {
+        this.termEndDate = termEndDate;
+    }
+
+    public String getTermStartDate() {
+        return termStartDate;
+    }
+
+    public void setTermStartDate(String termStartDate) {
+        this.termStartDate = termStartDate;
+    }
 }
