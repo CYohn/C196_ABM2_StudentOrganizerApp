638e3ad9b2b82c2cc683aafda901d604c238bec7
Charity Yohn <53451923+CYohn@users.noreply.github.com>
2022-11-26 23:58:01 -0500

    Created the files for the course detailed view: CourseDetailsActivity and activity_course_details.xml. Completed the layout for the course_list_item_layout.xml

---
+++ app/src/main/java/UI/CourseDetailsActivity.java
@@ -0,0 +1,16 @@
+package UI;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.zybooks.c196_abm2_charity_yohn.R;
+
+public class CourseDetailsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_course_details);
+    }
+}
