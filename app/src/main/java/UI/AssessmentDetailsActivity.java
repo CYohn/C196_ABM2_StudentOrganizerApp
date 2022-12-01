852838afbcf19d4ff0fefa6cf093ad8c198b9aba
Charity Yohn <53451923+CYohn@users.noreply.github.com>
2022-11-30 20:44:02 -0500

    passing the data from the selected assessment to the assessment details page is complete.

--- app/src/main/java/UI/AssessmentDetailsActivity.java
+++ app/src/main/java/UI/AssessmentDetailsActivity.java
@@ -1,16 +1,43 @@
 package UI;
 
 import android.os.Bundle;
+import android.widget.EditText;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.zybooks.c196_abm2_charity_yohn.R;
 
 public class AssessmentDetailsActivity extends AppCompatActivity {
+    EditText assessmentTitleEditTxt;
+    EditText assessmentStartEditTxt;
+    EditText assessmentEndDateEditTxt;
+    EditText assessmentTypeEditTxt;
+
+    String assessmentTitle;
+    String assessmentStartDate;
+    String assessmentEndDate;
+    String assessmentType;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_assessment_details);
+
+        assessmentTitleEditTxt=findViewById(R.id.assessmentTitleEditTxt);
+        assessmentStartEditTxt =findViewById(R.id.assessmentStartEditTxt);
+        assessmentEndDateEditTxt=findViewById(R.id.assessmentEndEditTxt);
+        assessmentTypeEditTxt=findViewById(R.id.assessmentTypeEditTxt);
+
+        assessmentTitle=getIntent().getStringExtra("assessmentTitleTxtView");
+        assessmentStartDate=getIntent().getStringExtra("assessmentStartTxtView");
+        assessmentEndDate=getIntent().getStringExtra("assessmentEndTxtView");
+        assessmentType=getIntent().getStringExtra("assessmentTypeTxtView");
+
+
+        assessmentTitleEditTxt.setText(assessmentTitle);
+        assessmentStartEditTxt.setText(assessmentStartDate);
+        assessmentEndDateEditTxt.setText(assessmentEndDate);
+        assessmentTypeEditTxt.setText(assessmentType);
     }
 }
