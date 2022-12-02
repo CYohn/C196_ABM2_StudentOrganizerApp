8d262b21c56228763c02c1abcd0406fb7b1c0dc0
Charity Yohn <53451923+CYohn@users.noreply.github.com>
2022-12-02 00:48:11 -0500

    Added the main navigation menu to each main activity.

--- app/src/main/java/UI/NotesActivity.java
+++ app/src/main/java/UI/NotesActivity.java
@@ -1,7 +1,12 @@
 package UI;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.FragmentTransaction;
 
@@ -17,5 +22,40 @@ public class NotesActivity extends AppCompatActivity {
         fragmentTransaction.replace(R.id.notesActivityFragmentViewer, new NotesFragment());
         fragmentTransaction.addToBackStack("NotesFragment");
         fragmentTransaction.commit();
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu){
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.main_navigation_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()){
+            case R.id.termsMenuOption:
+                Intent termsIntent = new Intent(this, TermsActivity.class);
+                startActivity(termsIntent);
+                return true;
+
+            case R.id.coursesMenuOption:
+                Intent courseIntent = new Intent(this, CourseActivity.class);
+                startActivity(courseIntent);
+                return true;
+
+
+            case R.id.assessmentsMenuOption:
+                Intent assessmentIntent = new Intent(this, AssessmentsActivity.class);
+                startActivity(assessmentIntent);
+                return true;
+
+            case R.id.notesMenuOption:
+                Intent notesIntent = new Intent(this, NotesActivity.class);
+                startActivity(notesIntent);
+                return true;
+
+            default:return super.onOptionsItemSelected(item);
+        }
     }
 }
