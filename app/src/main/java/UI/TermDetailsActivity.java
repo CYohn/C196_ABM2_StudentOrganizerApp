e965b8bf78907616b5c001a8ab912f0acccde243
Charity Yohn <53451923+CYohn@users.noreply.github.com>
2022-12-02 20:16:51 -0500

    Added main menus to the details files: AssessmentDetailsActivity, CourseDetailsActivity, NotesDetailsActivity, and TermDetailsActivity.

--- app/src/main/java/UI/TermDetailsActivity.java
+++ app/src/main/java/UI/TermDetailsActivity.java
@@ -1,8 +1,13 @@
 package UI;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.widget.EditText;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.zybooks.c196_abm2_charity_yohn.R;
@@ -35,5 +40,40 @@ public class TermDetailsActivity extends AppCompatActivity {
         termEndDateEditTxt.setText(termEndDate);
 
 
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu){
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.main_navigation_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()){
+            case R.id.termsMenuOption:
+                Intent termsIntent = new Intent(this, TermsActivity.class);
+                startActivity(termsIntent);
+                return true;
+
+            case R.id.coursesMenuOption:
+                Intent courseIntent = new Intent(this, CourseActivity.class);
+                startActivity(courseIntent);
+                return true;
+
+
+            case R.id.assessmentsMenuOption:
+                Intent assessmentIntent = new Intent(this, AssessmentsActivity.class);
+                startActivity(assessmentIntent);
+                return true;
+
+            case R.id.notesMenuOption:
+                Intent notesIntent = new Intent(this, NotesActivity.class);
+                startActivity(notesIntent);
+                return true;
+
+            default:return super.onOptionsItemSelected(item);
+        }
     }
 }
