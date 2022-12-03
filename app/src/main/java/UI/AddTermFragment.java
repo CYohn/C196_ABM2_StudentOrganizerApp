599ece6681ec8e645c9546ff1b99bd1f7d7a46ea
Charity Yohn <53451923+CYohn@users.noreply.github.com>
2022-12-02 20:38:34 -0500

    Added the button label to the button on main. Adjusted the fragment vier on activity_terms.xml.

--- app/src/main/java/UI/AddTermFragment.java
+++ app/src/main/java/UI/AddTermFragment.java
@@ -4,10 +4,8 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.ImageButton;
 
 import androidx.fragment.app.Fragment;
-import androidx.fragment.app.FragmentTransaction;
 
 import com.zybooks.c196_abm2_charity_yohn.R;
 
@@ -33,29 +31,10 @@ public class AddTermFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        View view = inflater.inflate(R.layout.fragment_add_term, container, false);
+        View view = inflater.inflate(R.layout.fragment_add_term, container, false);;
 
-        ImageButton addCoursesFromTermAddBtn = (ImageButton) view.findViewById(R.id.addCoursesFromTermAddBtn);
-        addCoursesFromTermAddBtn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                FragmentTransaction fragmentTransaction = getParentFragmentManager().beginTransaction();
-                fragmentTransaction.replace(R.id.termsFragmentHolder, new AddNewCourseFragment());
-                fragmentTransaction.addToBackStack("addInstructorView");
-                fragmentTransaction.commit();
-            }
-        });
 
-        ImageButton closeAddTermsBtn = (ImageButton) view.findViewById(R.id.closeAddTermsBtn);
-        closeAddTermsBtn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                FragmentTransaction fragmentTransaction = getParentFragmentManager().beginTransaction();
-                fragmentTransaction.replace(R.id.termsFragmentHolder, new AllTermsListFragment());
-                fragmentTransaction.addToBackStack("addNoteView");
-                fragmentTransaction.commit();
-            }
-        });
+        
 
         return view;
     }
