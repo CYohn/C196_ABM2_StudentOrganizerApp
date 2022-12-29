f4af5c188d972f8ff0672cf7171d7a574f669d80
Charity Yohn <53451923+CYohn@users.noreply.github.com>
2022-12-29 14:12:10 -0500

    Created the activity and files for viewing a list of instructors

---
+++ app/src/main/java/UI/InstructorAdapter.java
@@ -0,0 +1,112 @@
+package UI;
+
+import android.content.Context;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentActivity;
+import androidx.fragment.app.FragmentTransaction;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.zybooks.c196_abm2_charity_yohn.R;
+
+import java.util.List;
+
+import Entities.Instructor;
+import Entities.Note;
+
+public class InstructorAdapter extends RecyclerView.Adapter<InstructorAdapter.InstructorViewHolder> {
+
+    private List<Instructor> mInstructor;
+    private final Context context;
+    private final LayoutInflater mInflator;
+    InstructorAdapter(Context context){
+        mInflator= LayoutInflater.from(context);
+        this.context=context;
+    }
+
+
+    class InstructorViewHolder extends RecyclerView.ViewHolder{
+        private final TextView DBInstructorName;
+        private final TextView DBInstructorPhone;
+        private final TextView DBInstructorEmail;
+
+
+        private NoteViewHolder(View itemView){
+            super(itemView);
+            DBInstructorName = itemView.findViewById(R.id.instructorNameCardView);
+            DBInstructorEmail = itemView.findViewById(R.id.instructorEmailCardView);
+            DBInstructorPhone = itemView.findViewById(R.id.instructorPhoneCardView);
+
+            itemView.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    int position=getAdapterPosition();
+                    final Instructor selectedInstructor= mInstructor.get(position);
+
+                    Fragment instructorDetails = new InstructorFragment();
+                    FragmentTransaction fragmentTransaction = ((FragmentActivity) context).getSupportFragmentManager().beginTransaction();
+
+                    Bundle bundle = new Bundle();
+
+                    bundle.putString("instructorNameValue", selectedInstructor.getInstructorName());
+                    bundle.putString("instructorEmailValue", selectedInstructor.getInstructorEmail());
+                    bundle.putString("instructorPhoneValue", selectedInstructor.getInstructorPhone());
+
+                    instructorDetails.setArguments(bundle);
+                    fragmentTransaction.replace(R.id.notesActivityFragmentViewer, instructorDetails);
+                    fragmentTransaction.addToBackStack("NoteDetailFragment");
+                    fragmentTransaction.commit();
+                }
+            });
+        }
+    }
+
+
+    //Inflate the list item
+    @NonNull
+    @Override
+    public InstructorAdapter.InstructorViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View itemView=mInflator.inflate(R.layout.instructor_list_item_layout,parent,false);
+        return new InstructorViewHolder(itemView);
+    }
+
+    //Put things on the text view
+    @Override
+    public void onBindViewHolder(@NonNull NoteAdapter.NoteViewHolder holder, int position) {
+        if(mInstructor !=null){
+            Instructor selectedInstructor = mInstructor.get(position);
+            String name = selectedInstructor.getInstructorName();
+            String email = selectedInstructor.getInstructorEmail();
+            String phone = selectedInstructor.getInstructorPhone();
+
+            holder.DBInstructorName.setText(name);
+            holder.DBInstructorPhone.setText(phone);
+            holder.DBInstructorEmail.setText(email);
+
+        }
+        else{
+            holder.DBInsructorName.setText("No Instructor Found");
+            holder.DBInstructorPhone.setText("Phone");
+            holder.DBInstructorEmail.setText("Email");
+        }
+    }
+    public void setmInstructor(List<Instructor> instructors){
+        mInstructor =instructors;
+        notifyDataSetChanged();
+    }
+
+    @Override
+    public int getItemCount() {
+        if(mInstructor !=null){
+            return mInstructor.size();
+        }
+        else return 0;
+    }
+
+}
