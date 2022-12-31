4798d785086bba6945b4208b020afb261b8f3eec
Charity Yohn <53451923+CYohn@users.noreply.github.com>
2022-12-31 00:32:05 -0500

    Assessments is saving new assessments correctly (the title is not showing on the cardView)

--- app/src/main/java/UI/AssessmentDetailsFragment.java
+++ app/src/main/java/UI/AssessmentDetailsFragment.java
@@ -1,6 +1,11 @@
 package UI;
 
+import static java.util.Calendar.DAY_OF_MONTH;
+import static java.util.Calendar.MONTH;
+import static java.util.Calendar.YEAR;
+
 import android.app.AlertDialog;
+import android.app.DatePickerDialog;
 import android.content.Context;
 import android.content.DialogInterface;
 import android.os.Bundle;
@@ -9,6 +14,7 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.DatePicker;
 import android.widget.EditText;
 import android.widget.ImageButton;
 import android.widget.Spinner;
@@ -17,12 +23,19 @@ import android.widget.Toast;
 import android.widget.ToggleButton;
 
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentActivity;
+import androidx.fragment.app.FragmentTransaction;
 
 import com.zybooks.c196_abm2_charity_yohn.R;
 
 import org.jetbrains.annotations.Nullable;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.Locale;
 
 import Database.RepositoryForStudentOrganizer;
 import Entities.Assessment;
@@ -55,6 +68,10 @@ ImageButton deleteBtn;
 
 RepositoryForStudentOrganizer.Repository repo;
 ArrayList<Course> courseArrayList;
+final Calendar startDateCalendar = Calendar.getInstance();
+DatePickerDialog.OnDateSetListener startDateDialog;
+final Calendar endDateCalendar = Calendar.getInstance();
+DatePickerDialog.OnDateSetListener endDateDialog;
 
 Bundle bundle;
 
@@ -206,6 +223,118 @@ Bundle bundle;
                 }
             }
         });
+
+        saveBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Bundle bundle = getArguments();
+                associatedCourseSpinner = view.findViewById(R.id.assessmentAssociatedCourseSpinner);
+                Course selectedCourse = (Course) associatedCourseSpinner.getSelectedItem();
+                int courseId = selectedCourse.getCourseId();
+                String title = ((EditText) getView().findViewById(R.id.assessmentNameTxtinput)).getText().toString();
+                String endDate = endDateBtn.getText().toString();
+                String startDate = startDateBtn.getText().toString();
+                String assessmentType = assessmentTypeToggle.getText().toString();
+                if (bundle != null){
+                    assessmentId = bundle.getInt("assessmentId", -1);
+                } else{assessmentId = -1;}
+
+
+                //Save info to DB
+                Assessment assessment;
+
+                if (assessmentId == -1) {
+                    repo = new RepositoryForStudentOrganizer.Repository(getActivity().getApplication()); //Without this line, the program was throwing a null pointer exception for the repo
+                    int newId = repo.getAllAssessments().get(repo.getAllAssessments().size() - 1).getAssessmentId() + 1;//get the ID of the last term in the list
+                    assessment = new Assessment(newId, assessmentType, assessmentTitle, endDate, startDate, courseId);
+                    repo.insert(assessment);
+
+                    //Inform the user the assessment was saved
+                    Context context = getContext();
+                    CharSequence text = "Assessment saved successfully";
+                    int duration = Toast.LENGTH_LONG;
+                    Toast toast = Toast.makeText(context, text, duration);
+                    toast.show();
+                } else {
+
+                    if (bundle != null) {
+                        assessmentId = bundle.getInt("assessmentId", -1);
+                        assessment = new Assessment(assessmentId, assessmentType,
+                                assessmentTitle, endDate, startDate, courseId);
+                        repo.update(assessment);
+                        //Inform the user the note updated successfully
+                        Context context = getContext();
+                        CharSequence text = "Assessment updated successfully";
+                        int duration = Toast.LENGTH_LONG;
+
+                        Toast toast = Toast.makeText(context, text, duration);
+                        toast.show();
+                    }
+                }
+                Fragment assessmentList = new AllAssessmentsListFragment();
+                FragmentTransaction fragmentTransaction = ((FragmentActivity) getContext()).getSupportFragmentManager().beginTransaction();
+                fragmentTransaction.replace(R.id.assessmentFragmentContainerView, assessmentList);
+                fragmentTransaction.addToBackStack("AssessmentListFragment");
+                fragmentTransaction.commit();
+            }
+        });
+
+        startDateBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String dateFormat = "MM/dd/YY";
+                String dateString = startDateBtn.getText().toString();
+                SimpleDateFormat simpleDateFormat = new SimpleDateFormat(dateFormat, Locale.getDefault());
+                try {
+                    startDateCalendar.setTime(simpleDateFormat.parse(dateString));
+                } catch (ParseException dateParseException) {
+                    dateParseException.printStackTrace();
+                    dateParseException.getMessage();
+                    dateParseException.getCause();
+                }
+                new DatePickerDialog(getContext(), startDateDialog, startDateCalendar.get(YEAR),
+                        startDateCalendar.get(MONTH), startDateCalendar.get(DAY_OF_MONTH)).show();
+            }
+        });
+
+        startDateDialog = new DatePickerDialog.OnDateSetListener() {
+            @Override
+            public void onDateSet(DatePicker datePicker, int year, int monthOfYear, int dayOfMonth) {
+                startDateCalendar.set(YEAR, year);
+                startDateCalendar.set(MONTH, monthOfYear);
+                startDateCalendar.set(DAY_OF_MONTH, dayOfMonth);
+                updateStartDateLabel();
+            }
+        };
+
+        endDateBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String dateFormat = "MM/dd/YY";
+                String dateString = endDateBtn.getText().toString();
+                SimpleDateFormat simpleDateFormat = new SimpleDateFormat(dateFormat, Locale.getDefault());
+                try {
+                    endDateCalendar.setTime(simpleDateFormat.parse(dateString));
+                } catch (ParseException dateParseException) {
+                    dateParseException.printStackTrace();
+                    dateParseException.getMessage();
+                    dateParseException.getCause();
+                }
+                new DatePickerDialog(getContext(), endDateDialog, endDateCalendar.get(YEAR),
+                        endDateCalendar.get(MONTH), endDateCalendar.get(DAY_OF_MONTH)).show();
+            }
+        });
+
+        endDateDialog = new DatePickerDialog.OnDateSetListener() {
+            @Override
+            public void onDateSet(DatePicker datePicker, int year, int monthOfYear, int dayOfMonth) {
+                endDateCalendar.set(YEAR, year);
+                endDateCalendar.set(MONTH, monthOfYear);
+                endDateCalendar.set(DAY_OF_MONTH, dayOfMonth);
+                updateEndDateLabel();
+            }
+        };
     }
 
 
@@ -221,4 +350,20 @@ Bundle bundle;
         }
         return -1;
     };
+
+    private void updateStartDateLabel() {
+        String dateFormat = "MM/dd/YY";
+        SimpleDateFormat simpleDateFormat = new SimpleDateFormat(dateFormat, Locale.getDefault());
+        String currentDate = simpleDateFormat.format(new Date());
+        startDateBtn = (Button) getView().findViewById(R.id.assessmentStartDateBtn);
+        startDateBtn.setText(simpleDateFormat.format(startDateCalendar.getTime()));
+    }
+
+    private void updateEndDateLabel() {
+        String dateFormat = "MM/dd/YY";
+        SimpleDateFormat simpleDateFormat = new SimpleDateFormat(dateFormat, Locale.getDefault());
+        String currentDate = simpleDateFormat.format(new Date());
+        endDateBtn = (Button) getView().findViewById(R.id.assessmentEndDateBtn);
+        endDateBtn.setText(simpleDateFormat.format(endDateCalendar.getTime()));
+    }
 }
